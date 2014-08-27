d920f1deda2c19d538c52664009442bdd3454e2d
crstnbr <crstnbr@googlemail.com>
2014-08-27 11:56:39 +0200

    Several modifications (Philip?)

--- sagenb/data/sage/js/translated-messages.js
+++ sagenb/data/sage/js/translated-messages.js
@@ -1,22 +1,22 @@
 {#
  Dummy list of strings to be translated (for pybabel). Here because pybabel's parser can't parse localization.js
 #}
-{{ gettext("Click to download and install tex fonts.") }}
+{{ gettext('Click to download and install tex fonts.') }}
 {{ gettext('Your browser / OS combination is not supported.\\nPlease use Firefox or Opera under Linux, Windows, or Mac OS X, or Safari.') }}
-{{ gettext("Java Applet Hidden") }}
-{{ gettext("Click here to pop out") }}
+{{ gettext('Java Applet Hidden') }}
+{{ gettext('Click here to pop out') }}
 {{ gettext('Error applying function to worksheet(s).') }}
 {{ gettext('Title of saved worksheet') }}
 {{ gettext('Failed to save worksheet.') }}
-{{ gettext("Rename worksheet") }}
-{{ gettext("Please enter a name for this worksheet.") }}
-{{ gettext("Rename") }}
+{{ gettext('Rename worksheet') }}
+{{ gettext('Please enter a name for this worksheet.') }}
+{{ gettext('Rename') }}
 {{ gettext('Possible failure deleting worksheet.') }}
-{{ gettext("unprinted") }}
+{{ gettext('unprinted') }}
 {{ gettext('You requested to evaluate a cell that, for some reason, the server is unaware of.') }}
-{{ gettext("Error") }}
+{{ gettext('Error') }}
 {{ gettext('This worksheet is read only. Please make a copy or contact the owner to change it.') }}
-{{ gettext("loading...") }}
+{{ gettext('loading...') }}
 {{ gettext('Error updating cell output after ') }}
 {{ gettext('s (canceling further update checks).') }}
 {{ gettext('Problem inserting new input cell after current input cell.\\n') }}
@@ -24,12 +24,12 @@
 {{ gettext('Unable to interrupt calculation.') }}
 {{ gettext('Close this box to stop trying.') }}
 {{ gettext('Interrupt attempt') }}
-{{ gettext("<a href='javascript:restart_sage();'>Restart</a>, instead?") }}
-{{ gettext("Emptying the trash will permanently delete all items in the trash. Continue?") }}
-{{ gettext("Get Image") }}
+{{ gettext('<a href="javascript:restart_sage();">Restart</a>, instead?') }}
+{{ gettext('Emptying the trash will permanently delete all items in the trash. Continue?') }}
+{{ gettext('Get Image') }}
 {{ gettext('Jmol Image') }}
-{{ gettext("To save this image, you can try right-clicking on the image to copy it or save it to a file, or you may be able to just drag the image to your desktop.") }}
-{{ gettext("Sorry, but you need a browser that supports the &lt;canvas&gt; tag.") }}
+{{ gettext('To save this image, you can try right-clicking on the image to copy it or save it to a file, or you may be able to just drag the image to your desktop.') }}
+{{ gettext('Sorry, but you need a browser that supports the &lt;canvas&gt; tag.') }}
 {{ ngettext('Trying again in %(num)d second...', 'Trying again in %(num)d seconds...', n) }}
 {{ gettext('Login') }}
 {{ gettext('Home') }}
@@ -41,25 +41,25 @@
 {{ gettext('Help') }}
 {{ gettext('Report a Problem') }}
 {{ gettext('collaborators') }}
-{{ gettext("Evaluate") }}
-{{ gettext("Show Static Image") }}
-{{ gettext("Popout") }}
-{{ gettext("Open Interactive View") }}
-{{ gettext("Untitled") }}
-{{ gettext("Are you sure you would like to interrupt the running computation?") }}
-{{ gettext("Copy to clipboard: ") }}
-{{ gettext("running") }}
+{{ gettext('Evaluate') }}
+{{ gettext('Show Static Image') }}
+{{ gettext('Popout') }}
+{{ gettext('Open Interactive View') }}
+{{ gettext('Untitled') }}
+{{ gettext('Are you sure you would like to interrupt the running computation?') }}
+{{ gettext('Copy to clipboard: ') }}
+{{ gettext('running') }}
 {{ gettext('other(s)') }}
-{{ gettext("ago") }}
-{{ gettext("Emptying the Trash is final. Are you sure?") }}
-{{ gettext("Nothing here!") }}
-{{ gettext("Published Worksheets") }}
-{{ gettext("My Notebook") }}
-{{ gettext("Archive") }}
-{{ gettext("Trash") }}
-{{ gettext("Published Worksheets") }}
-{{ gettext("Active") }}
-{{ gettext("Search") }}
+{{ gettext('ago') }}
+{{ gettext('Emptying the Trash is final. Are you sure?') }}
+{{ gettext('Nothing here!') }}
+{{ gettext('Published Worksheets') }}
+{{ gettext('My Notebook') }}
+{{ gettext('Archive') }}
+{{ gettext('Trash') }}
+{{ gettext('Published Worksheets') }}
+{{ gettext('Active') }}
+{{ gettext('Search') }}
 {{ gettext('close') }}
 {{ gettext('open') }}
 {{ gettext('Open chat') }}
@@ -72,3 +72,4 @@
 {{ gettext('joined') }}
 {{ gettext('left') }}
 {{ gettext('and') }}
+{{ gettext(' is working on this cell. Therefore it has been locked.') }}
